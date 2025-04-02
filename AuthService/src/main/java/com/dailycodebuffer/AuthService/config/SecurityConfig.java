2adcff2f15c2dc498990b64c4020b66181f1c931
walunja1 <walunja1@gmail.com>
2025-04-02 18:06:28 +0530

    Fifth change
    Signed-off-by: walunja1 <walunja1@gmail.com>

---
+++ AuthService/src/main/java/com/dailycodebuffer/AuthService/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.dailycodebuffer.AuthService.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("http://localhost:5173/home", true));
+
+
+        return http.build();
+    }
+
+}
